606c1cfbae09f3fff7a9c9ee4ab77c2b80e1678b
Felix Hillingshaeuser <felix@mxcore.de>
2020-05-17 18:33:45 +0200

    add day01 part 2

--- day01/main.go
+++ day01/main.go
@@ -6,7 +6,7 @@ import (
 	"os"
 )
 
-func sumOfLine(input string) int {
+func sumOfLine1(input string) int {
 	var sum int
 	len := len(input)
 	for i := 0; i < len; i++ {
@@ -23,12 +23,30 @@ func sumOfLine(input string) int {
 	return sum
 }
 
+func sumOfLine2(input string) int {
+	var sum int
+	len := len(input)
+	half := len / 2
+	for i := 0; i < len; i++ {
+		var j int
+		j = i + half
+		if j >= len {
+			j = j - len
+		}
+		if input[i] == input[j] {
+			sum += int(input[i]) - 48
+		}
+	}
+	return sum
+}
+
 func main() {
 	file, _ := os.Open("input.txt")
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(sumOfLine(line))
+		fmt.Println(sumOfLine1(line))
+		fmt.Println(sumOfLine2(line))
 	}
 }
