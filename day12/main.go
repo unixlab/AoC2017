d799c0f8f4ac7c80011eacaec38fca41d9d02c57
Felix Hillingshaeuser <felix@mxcore.de>
2022-06-10 16:51:25 +0200

    add day12 part 2

--- day12/main.go
+++ day12/main.go
@@ -30,6 +30,26 @@ func (ia IntArray) Remove(n int) IntArray {
 	return newIA
 }
 
+func (ia IntArray) Contains(n int) bool {
+	for _, v := range ia {
+		if v == n {
+			return true
+		}
+	}
+	return false
+}
+
+func isInProgram(p []IntArray, ia IntArray) bool {
+	for _, v := range p {
+		for _, v2 := range ia {
+			if v.Contains(v2) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
 func countConnects(input map[int]IntArray, start int) int {
 	counter := len(input[start])
 	walk := input[start]
@@ -43,8 +63,24 @@ func countConnects(input map[int]IntArray, start int) int {
 	return counter
 }
 
+func getPath(input map[int]IntArray, start int) []int {
+	var path []int
+	walk := input[start]
+	delete(input, start)
+	path = append(path, start)
+	for k, v := range input {
+		input[k] = v.Remove(start)
+	}
+	for _, w := range walk {
+		path = append(path, getPath(input, w)...)
+	}
+	return path
+}
+
 func main() {
 	inputs := make(map[int]IntArray, 2000)
+	inputsPart1 := make(map[int]IntArray, 2000)
+	inputsPart2 := make(map[int]IntArray, 2000)
 
 	file, _ := os.Open("input.txt")
 	scanner := bufio.NewScanner(file)
@@ -70,5 +106,24 @@ func main() {
 		}
 	}
 
-	fmt.Println(countConnects(inputs, 0))
+	for k, v := range inputs {
+		inputsPart1[k] = v
+		inputsPart2[k] = v
+	}
+
+	fmt.Printf("part 1 => %d\n", countConnects(inputsPart1, 0))
+
+	var programs []IntArray
+	for i, _ := range inputs {
+		path := getPath(inputsPart2, i)
+		var ia IntArray
+		for _, v := range path {
+			ia = ia.AddUnique(v)
+		}
+		if !isInProgram(programs, ia) {
+			programs = append(programs, ia)
+		}
+	}
+
+	fmt.Printf("part 2 => %d\n", len(programs))
 }
